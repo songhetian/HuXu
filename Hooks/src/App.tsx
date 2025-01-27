53d5aa13818f0d13c549771ebf9a85074e0ef404
song <419097924@qq.com>
2025-01-27 17:15:05 +0800

    formAction

--- Hooks/src/App.tsx
+++ Hooks/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+// function Action(formData: any) {
+//   const name = formData.get("name");
+//   console.log(name);
+// }
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {/* <form action={Action}>
+        姓名:
+        <input
+          type="text"
+          name="name"
+          defaultValue="a123"
+        />
+        <input
+          type="submit"
+          value="提交"
+        />
+      </form> */}
 
-export default App
+      <form></form>
+    </div>
+  );
+};
+
+export default App;
